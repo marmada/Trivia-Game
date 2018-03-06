1ac4eb30768c1c7e56945df69658e97142784941
Miriam Armada <miriam_armada@yahoo.com>
2018-03-05 17:42:08 -0700

    functions almost complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,89 @@
 
 // Timer Set up 
 
-var time=30;
+var time = 120;
 var clock;
 
 
 function run() {
     clearInterval(clock);
     clock = setInterval(decrement, 1000);
-  }
+    
+}
 function decrement() {
-    //  Decrease number by one.
+    //   Decrease number by one.
     time--;
-    //  Show the number in the #show-number tag.
-    $("#timer").html("<h2>" + time + "</h2>");
+    console.log(time);
 
-    if (time===0) {
+    var convTime = timeConverter(time);
 
-        stopT(); // move to next question or GAMEOVER
+    $("#timer").html("<h2>" + convTime + "</h2>");
+
+    if (time === 0) {
+
+        stopT();
     }
-    else{ return;
+    else {
+        return;
     }
 }
 function stopT() {
     clearInterval(clock);
 }
 
+function timeConverter(t) {
 
-var unAns= 10
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    }
+    else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
+
+
+
+var unAns = 10
 var answC = 0;
 var incorrC = 0;
 
+function game() {
+    $("input").on("click", function () {
 
-$(document).ready(function(){
+        var ansQ = $("input:checked").val();
+        console.log(ansQ);
 
-function game(){
-$("input").on("click", function(){
+        if (ansQ == "1") {
+            answC++;
+            unAns--;
+            $("correct").text("Correct:" + answC);
 
-    var ansQ = $("input:checked").val();
-    console.log(ansQ);
+        }
 
-if (ansQ =="1") {
-    answC++;
-    unAns--;
+        else {
+            incorrC++;
+            unAns--;
+        }
+
+    });
+
+}
+        $(document).ready(function () {
+
+
+        });
     
-  }
-
-  else{
-    incorrC++;
-    unAns--;
-  }
 
 
-});}
-
-});
 
 
 
